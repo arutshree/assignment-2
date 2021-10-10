8ac16158f10017ec2d8dd6724eed5a9fc3b582eb
arutshree <91821172+arutshree@users.noreply.github.com>
2021-10-11 00:04:22 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+alphabets="abcdefghijklmnopqrstuvwxyz"
+ascii_dict={}
+for i in alphabets:
+    ascii_dict[i]=ord(i)
+
+
+# In[2]:
+
+
+print(ascii_dict)
+
+
+# In[ ]:
+
+
+
+
